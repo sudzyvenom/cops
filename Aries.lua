66140f7fa7bf303538ae6180edeabce5948078f5
sudzyvenom <60900268+sudzyvenom@users.noreply.github.com>
2021-02-24 21:26:59 +0300

    Update Aries.lua

--- Aries.lua
+++ Aries.lua
@@ -1,13 +1,3 @@
-local ScriptVer     = "1.0 [VIP]"
-local ScriptName	= "Aries"
-local ScriptCfg		= gg.getFile()..".ariescfg"
-
-loadCfg = loadfile(ScriptCfg) User = "" Remember = false
-if loadCfg ~= nil then 
-    local CfgTable = loadCfg()
-	User		= CfgTable[1]
-	Remember	= CfgTable[2]
-end
 
 radarstate = "【OFF】"
 recoilstate = "【OFF】"
@@ -411,35 +401,6 @@ gg.getResults(1000)
 gg.editAll('-120',gg.TYPE_FLOAT)
 gg.clearResults()
 gg.toast("💚 TR UG Activated 💚 ")
-end
-
-local Token = {"Grax"}
-
-while true do
-if gg.isVisible(true) then gg.setVisible(false)
-if string.len(User) ~= 0 then 
-deneme = gg.alert("⚡ Continue as "..User.." ?", "⭕ Yes", "✖️ No")
-			if deneme  == 1 then login1 = {User, Remember} m32() break end
-			if deneme  == 2 then os.remove(ScriptCfg) os.exit() break end
-
-       end
-login1 = gg.prompt({ScriptName..' v'..ScriptVer..'\n~ Discord: Grax#7844 \nNote: Please do not share your token with other.\n'
-.."👥 Token:", "Remember Me ?"}, {User, Remember}, {"text", "checkbox"})
-if login1 == nil then
-os.remove(ScriptCfg)
-gg.alert("Invalid Token !\nMake sure your token not empty.") os.exit()
-break
-end
-if login1[1] == Token[1] then
-gg.saveVariable({(login1[2] == true and login1[1] or ""), login1[2]}, ScriptCfg)
-gg.toast("Welcome user.")
-m32()
-else
-os.exit()
-end
-break
-
-end
 end
 
 while true do
